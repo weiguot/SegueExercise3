a459a919483098e604e472a6ba37236b991f2056
Weiguo Tao <weiguotao12@gmail.com>
2021-02-16 00:06:48 -0500

    Initial Commit

---
+++ SegueExercise3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueExercise3
+//
+//  Created by Field Employee on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
